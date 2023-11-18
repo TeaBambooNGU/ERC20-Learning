93b4f59dcc057d355a8dad787fadf19218b53623
tea bamboo <teabamboongu@gmail.com>
2023-11-18 14:56:29 +0800

    zz

---
+++ src/TangToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.20;
+import "solmate/tokens/ERC20.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+error NoPayMintPrice();
+error WithdrawTransfer();
+error MaxSupply();
+contract SDUFECoin is ERC20,Ownable {
+    uint256 public constant MINT_PRICE = 0.00000001 ether;
+    uint256 public constant MAX_SUPPLY = 1_000_000_000;
+
+    constructor (
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        address _initOwner
+    ) ERC20 (_name,_symbol,_decimals) Ownable(_initOwner){}
+
+    function mintTo(address recipient) public payable{
+
+    }
+}
+
+
+
