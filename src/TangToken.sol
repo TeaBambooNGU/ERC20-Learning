4c80dcedc2defe0cc937ff059c58931204963d8a
tea bamboo <teabamboongu@gmail.com>
2023-11-18 14:59:00 +0800

    修改

--- src/TangToken.sol
+++ src/TangToken.sol
@@ -5,7 +5,7 @@ import "openzeppelin-contracts/contracts/access/Ownable.sol";
 error NoPayMintPrice();
 error WithdrawTransfer();
 error MaxSupply();
-contract SDUFECoin is ERC20,Ownable {
+contract TangToken is ERC20,Ownable {
     uint256 public constant MINT_PRICE = 0.00000001 ether;
     uint256 public constant MAX_SUPPLY = 1_000_000_000;
 
