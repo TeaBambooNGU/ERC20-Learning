89195782b6abb21b5dc68c2b05e26be39bc746d0
tea bamboo <teabamboongu@gmail.com>
2023-11-19 13:14:08 +0800

    合约脚本开发

---
+++ script/TangToken.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/TangToken.sol";
+
+contract TangTokenScript is Script {
+    function setUp() public {}
+
+    function run() external {
+        
+    }
+}
